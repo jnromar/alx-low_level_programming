10e4387b2e20c7e6c215f8fcfbc12924efb3757d
jnromar <kennetiks@gmail.com>
2022-11-05 00:06:41 +0000

    Dreams feel real

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,15 +3,16 @@
  * Auth: omar
  */
 
-#include "main.h"  
+#include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- * Return: The length of the string
- */
-int _strlen_recursion(char *s)  
+* _strlen_recursion - Returns the length of a string.
+* @s: The string to be measured.
+* Return: The length of the string
+*/
+int _strlen_recursion(char *s)
 {
 	int len = 0;
+
 	if (*s)
 	{
 		len++;
